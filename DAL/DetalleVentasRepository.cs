e7172b53e51ff8263090a5981ea7e4ca9a800fc1
Clegro2003 <carloslegro6@gmail.com>
2025-05-22 10:51:11 -0500

    C y P

--- DAL/DetalleVentasRepository.cs
+++ DAL/DetalleVentasRepository.cs
@@ -11,10 +11,11 @@ namespace DAL
         {
             List<DetallesVentas> lista = new List<DetallesVentas>();
             string sentencia = @"
-                SELECT idventa, nombreproducto, cantidadlibras, preciolibra, totalventa, monto, vueltos,
-                       nombrecliente, apellidocliente, fechafactura, metododepago, idfactura
-                FROM postgres.""FullGestion"".vistaventas 
-                ORDER BY idventa";
+        SELECT idventa, id_producto, nombre_producto, cantidadlibras, preciolibra, totalventa,
+               nombrecliente, apellidocliente, id_factura, fecha_factura, total, monto, vueltos,
+               id_metodopago, metododepago
+        FROM postgres.""FullGestion"".vistaventas 
+        ORDER BY idventa";
 
             NpgsqlCommand cmd = new NpgsqlCommand(sentencia, conexion);
             AbrirConexion();
@@ -25,22 +26,26 @@ namespace DAL
                 lista.Add(new DetallesVentas
                 {
                     IDVenta = reader.GetInt32(reader.GetOrdinal("idventa")),
-                    NombreProducto = reader.GetString(reader.GetOrdinal("nombreproducto")),
+                    IDProducto = reader.GetInt32(reader.GetOrdinal("id_producto")),
+                    NombreProducto = reader.GetString(reader.GetOrdinal("nombre_producto")),
                     CantidadLibras = reader.GetInt32(reader.GetOrdinal("cantidadlibras")),
                     PrecioLibra = reader.GetDecimal(reader.GetOrdinal("preciolibra")),
                     TotalVenta = reader.GetDecimal(reader.GetOrdinal("totalventa")),
-                    Monto = reader.GetDecimal(reader.GetOrdinal("monto")),
-                    Vueltos = reader.GetDecimal(reader.GetOrdinal("vueltos")),
                     NombreCliente = reader.GetString(reader.GetOrdinal("nombrecliente")),
                     ApellidoCliente = reader.GetString(reader.GetOrdinal("apellidocliente")),
-                    FechaFactura = reader.GetDateTime(reader.GetOrdinal("fechafactura")),
-                    MetodoDePago = reader.GetString(reader.GetOrdinal("metododepago")),
-                    IDFactura = reader.GetInt32(reader.GetOrdinal("idfactura"))
+                    IDFactura = reader.GetInt32(reader.GetOrdinal("id_factura")),
+                    FechaFactura = reader.GetDateTime(reader.GetOrdinal("fecha_factura")),
+                    Total = reader.GetDecimal(reader.GetOrdinal("total")),
+                    Monto = reader.GetDecimal(reader.GetOrdinal("monto")),
+                    Vueltos = reader.GetDecimal(reader.GetOrdinal("vueltos")),
+                    IDMetodoPago = reader.GetInt32(reader.GetOrdinal("id_metodopago")),
+                    MetodoDePago = reader.GetString(reader.GetOrdinal("metododepago"))
                 });
             }
 
             CerrarConexion();
             return lista;
         }
+
     }
 }
